95d6678931662618472674703c5c9f6163fbe08a
Alberto <alnavarro@d1stkfactory>
2014-05-23 17:58:01 +0000

    FirstTask Excersice 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,21 @@ grunt.initConfig({
         release: {
             files: [ { src: './**', dest: './dist/' } ]
         }
+    },
+    version: {
+            options: {
+                        buildNumber: "1.0." + (+new Date())
+                            }
     }
 });
 
+grunt.registerTask('version', 'Add build number to the package.json', function() {
+var package = JSON.parse(grunt.file.read('./package.json'));
+package.version = this.options().buildNumber;
 
-grunt.registerTask('default', ['clean', 'copy']);
+grunt.file.write('./dist/package.json', JSON.stringify(package, null, 2));
+});
 
+
+grunt.registerTask('default', ['clean', 'copy', 'version']);
 };
